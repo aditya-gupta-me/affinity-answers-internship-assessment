3b6e2272434ef39879c376b443848ced22661a48
the-great-adee <adityagupta46772@gmail.com>
2025-11-27 15:11:16 +0530

    update: rfam_db_queries.sql file

--- 2-rfam-sql-queries/rfam_db_queries.sql
+++ 2-rfam-sql-queries/rfam_db_queries.sql
@@ -58,3 +58,15 @@ Question d) Pagination Query
 - Return the 9th page when there are 15 results per page
 (Hint: Include family accession ID, family name, and maximum length)
 */
+
+SELECT
+    f.rfam_acc,
+    f.rfam_id AS family_name,
+    MAX(rf.length) AS max_length
+FROM family f
+JOIN full_region fr ON f.rfam_acc = fr.rfam_acc
+JOIN rfamseq rf ON fr.rfamseq_acc = rf.rfamseq_acc
+GROUP BY f.rfam_acc, f.rfam_id
+HAVING MAX(rf.length) > 1000000
+ORDER BY max_length DESC
+LIMIT 15 OFFSET 120;
